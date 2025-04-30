b027ce2fb3cb2ba750997269c62374b165af0dc0
unknown <xbalog@ad.fi.muni.cz>
2025-04-30 10:51:28 +0200

    fix: codeQL

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -22,6 +22,7 @@ def print_nametag(format_string, person):
 
 
 def fetch_website(urllib_version, url):
+    assert(urlib_version != None)
     # Import the requested version (2 or 3) of urllib
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
